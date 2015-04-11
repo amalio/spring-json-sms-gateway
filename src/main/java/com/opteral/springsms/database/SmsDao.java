d099ea89f322ed0a4e994ac0777d23647b693b3a
amalio <amalio@opteral.com>
2015-04-11 11:18:00 +0200

    git core.ignorecase fixed

---
+++ src/main/java/com/opteral/springsms/database/SmsDao.java
@@ -0,0 +1,18 @@
+package com.opteral.springsms.database;
+
+import com.opteral.springsms.exceptions.GatewayException;
+import com.opteral.springsms.model.ACK;
+import com.opteral.springsms.model.SMS;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface SmsDao {
+    void insert(SMS sms) throws GatewayException;
+    void update(SMS sms) throws GatewayException;
+    void delete(SMS sms) throws GatewayException;
+    SMS getSMS(long id) throws GatewayException;
+    void updateSMS_Status(ACK ack) throws GatewayException;
+    List<SMS> getSMSForSend(Date aFecha) throws GatewayException;
+    SMS getSMSfromIdSMSC(String idSMSC) throws GatewayException;
+}
